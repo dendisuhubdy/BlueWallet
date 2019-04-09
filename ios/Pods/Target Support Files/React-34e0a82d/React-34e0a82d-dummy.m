7844d0afe604bc0db3ff0bcab8b8afae50e4fb1b
Marcos Rodriguez Vélez <marcospr@pm.me>
2019-04-08 23:35:10 -0400

    T

---
+++ ios/Pods/Target Support Files/React-34e0a82d/React-34e0a82d-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_React_34e0a82d : NSObject
+@end
+@implementation PodsDummy_React_34e0a82d
+@end
---
+++ ios/Pods/Target Support Files/React-a6220f23/React-a6220f23-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_React_a6220f23 : NSObject
+@end
+@implementation PodsDummy_React_a6220f23
+@end
